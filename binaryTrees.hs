6d2be3ed4c8a2a66b25b68aea2426edcac8b2c0f
jed1337 <j.caychingco@yahoo.com>
2020-09-22 18:07:27 +0800

    Add search, minimum, and maximum functions

--- binaryTrees.hs
+++ binaryTrees.hs
@@ -4,10 +4,13 @@ data Tree a = Empty
             
 leaf a = Node a Empty Empty
             
+-- variables
 tree3 = Node 2 (leaf 1) (leaf 3)
 tree7 = Node 4 (Node 2 (leaf 1) (leaf 3)) (Node 6 (leaf 5) (leaf 7))
+leftSkew' = Node 3 (Node 2 (leaf 1) Empty) Empty	
 leftSkew = Node 4 (Node 3 (Node 2 (leaf 1) Empty) Empty) Empty
 
+-- methods
 depth (Empty) = 0
 depth (Node _ left right) = 1+ max (depth left) (depth right)
 
@@ -34,3 +37,31 @@ createBST (x:xs) = createBST2 (Node x Empty Empty) xs
 
 createBST' tree [] = tree
 createBST' tree (x:xs) = createBST' (insert tree x) xs
+
+--1           2 
+--  2   ->   1 3
+--    3         
+rotateLeft Empty = Empty
+rotateLeft (Node value left (Node rightValue rightLeft rightRight)) =
+	(Node rightValue (Node value left rightLeft) rightRight)
+	
+--    3       2 
+--  2   ->   1 3
+--1             
+rotateRight Empty = Empty
+rotateRight (Node value (Node leftValue leftLeft leftRight) right) =
+	(Node leftValue leftLeft (Node value leftRight right))
+	
+searchBST Empty search = False
+searchBST (Node value left right) search 
+	| search == value = True
+	| search < value = searchBST left search
+	| search > value = searchBST right search
+	
+minimumBST Empty = error "No minimum value on an Empty tree"
+minimumBST (Node value Empty _) = value
+minimumBST (Node value left _) = minimumBST left
+
+maximumBST Empty = error "No maximum value on an Empty tree"
+maximumBST (Node value _ Empty) = value
+maximumBST (Node value _ right) = maximumBST right
--- readme.md
+++ readme.md
@@ -9,10 +9,14 @@ An array is returned containing the traversal order
 * Pre order
 * Post order
 
-## Additional functions
-* `Insert tree toInsert` Insert `toInsert` as a leaf into the binary seasrch tree `tree`
+## Functions
+* `Insert tree toInsert` Insert `toInsert` as a leaf into the binary search tree `tree`
 * `createBST xs` Create a binary search tree whose keys are the values of array `xs`
 * `createBST' tree xs` Create a binary search tree given an existing tree `tree` and insert the values of array `xs` as its key
+* `rotateLeft tree` rotate the tree `tree` to the left along the root node
+* `rotateRight tree` rotate the tree `tree` to the right along the root node
+* `minimumBST tree` return the minimum element in the tree `tree`
+* `maximumBST tree` return the maximum element in the tree `tree`
 
 ## Specs
 * GHC 8.6.5
